566d9d33737072a8328d14a8ffe065d72a7a8fbb
Nikita <system.program.code@gmail.com>
2020-10-17 17:53:37 +0300

    Настроил обучение пользования контроллерами

---
+++ Assets/Scripts/IndicationOff.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicationOff : MonoBehaviour
+{
+    public Animator IndicationLeft, IndicationRight;
+    public GameObject LocalAvatar, LeftHand, RightHand;
+
+    private void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.Any))
+        {
+            StartCoroutine(StopIndication());
+            StartCoroutine(ReplacementHand());
+        }
+    }
+
+    IEnumerator StopIndication()
+    {
+        yield return new WaitForSeconds(1f);
+        IndicationLeft.SetTrigger("Off");
+        IndicationRight.SetTrigger("Off");
+    }
+
+    IEnumerator ReplacementHand()
+    {
+        yield return new WaitForSeconds(2f);
+        LocalAvatar.SetActive(false);
+        LeftHand.SetActive(true);
+        RightHand.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/LookAtUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtUI : MonoBehaviour
+{
+    Transform m_centerEyeAnchor;
+
+    private void Start()
+    {
+        m_centerEyeAnchor = GameObject.Find("CenterEyeAnchor").transform;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(m_centerEyeAnchor);
+    }
+}
